e57457c85870ecbbd445f9ca33253f1323c3ec40
Nisha James P <nisha@agiline.com>
2017-04-08 12:31:11 +0530

    nisha

--- shruti_app/Nisha/employees/scripts/controllers/edit.ctrl.js
+++ shruti_app/Nisha/employees/scripts/controllers/edit.ctrl.js
@@ -1,70 +1,12 @@
-angular.module('myApp').controller('HomeController',function($scope,$location, $rootScope){
-	console.log('inside home');
-  $scope.logout = function()
-  {
-	  
-	  $location.path('/');
-  }
-	$rootScope.employees =[
-		{
-		"empId":"A1001",
-		"firstName":"Emmanuel",
-		"lastName":"Mathew",
-		"designmation":"Director",
-		"departmant":"Managment",
-		"age":"41",
-		"sex":"Male",
-		"mobile":"123456789",
-		"emailAddress":"emmanuel@gmail.com",
-		"location":"US"
-		},
-		{
-		"empId":"A1002",
-		"firstName":"Jim",
-		"lastName":"Alaxander",
-		"designmation":"Technical Architect",
-		"departmant":"Software",
-		"age":"32",
-		"sex":"Male",
-		"mobile":"123456789",
-		"emailAddress":"jim@gmail.com",
-		"location":"US"
-		},
-		{
-		"empId":"A1003",
-		"firstName":"Stefanus",
-		"lastName":"Budhipramano",
-		"designmation":"Lead Manager",
-		"departmant":"Datacenter Operatons",
-		"age":"37",
-		"sex":"Male",
-		"mobile":"123456789",
-		"emailAddress":"stefaus@gmail.com",
-		"location":"US"
-		},
-		{
-		"empId":"A1004",
-		"firstName":"Allen",
-		"lastName":"Hannan",
-		"designmation":"System Administrator",
-		"departmant":"Software",
-		"age":"24",
-		"sex":"Male",
-		"mobile":"123456789",
-		"emailAddress":"allen@gmail.com",
-		"location":"US"
-		},
-		{
-		"empId":"A1005",
-		"firstName":"Andrew",
-		"lastName":"Nugent",
-		"designmation":"Developer",
-		"departmant":"Software",
-		"age":"26",
-		"sex":"Male",
-		"mobile":"123456789",
-		"emailAddress":"andrew@gmail.com",
-		"location":"US"
-		},
-	]
+angular.module('myApp').controller('EditController',function($scope,$location, $routeParams, $rootScope){
+	console.log('inside edit');
+ var id = $routeParams.eId;
+	for(i=0; i< $rootScope.employees.length;i++){
+		if($rootScope.employees[i].empId == id ){
+			$scope.employee = $rootScope.employees[i];
+		}
+	}
+	$scope.goHome =function(){
+		$location.path('/home');
+	}
 });
